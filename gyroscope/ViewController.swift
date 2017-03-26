ac712343e50ec33da4dd27a06e4f2e75b97a25e4
Pieterjan Criel <crielpieterjan@gmail.com>
2017-03-26 16:36:15 +0200

    Initial Commit

---
+++ gyroscope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gyroscope
+//
+//  Created by Pieterjan Criel on 26/03/17.
+//  Copyright © 2017 Pieterjan Criel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
